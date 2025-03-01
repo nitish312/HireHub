3be5b486acdc1e7ee5d188921f206631c2c433bf
“nitish312” <nitishchintakindi@gmail.com>
2025-03-01 19:20:06 +0530

    html and css added

--- src/main/java/com/nitish/hirehub/HirehubApplication.java
+++ src/main/java/com/nitish/hirehub/HirehubApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HirehubApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HirehubApplication.class, args);
 	}
 
