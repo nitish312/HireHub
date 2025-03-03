0dc5b47a81b176b1898b34f536af9cac2be98fe2
nitish312 <nitishchintakindi@gmail.com>
2025-03-03 14:59:31 +0530

    recruiter and job seeker profile entity created

---
+++ src/main/java/com/nitish/hirehub/entity/JonSeekerProfile.java
@@ -0,0 +1,169 @@
+package com.nitish.hirehub.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "job_seeker_profile")
+public class JonSeekerProfile {
+
+    @Id
+    private int userAccountId;
+
+    @OneToOne
+    @JoinColumn(name = "userAccount_id")
+    @MapsId
+    private Users userId;
+
+    private String firstName;
+    private String lastName;
+    private String city;
+    private String state;
+    private String country;
+    private String workAuthorizatoin;
+    private String employeeType;
+    private String resume;
+
+    @Column(nullable = true, length = 64)
+    private String profilePhoto;
+
+    @OneToMany(targetEntity = Skills.class, cascade = CascadeType.ALL, mappedBy = "jobSeekerProfile")
+    private List<Skills> skills;
+
+    public JonSeekerProfile() {
+    }
+
+    public JonSeekerProfile(Users userId) {
+        this.userId = userId;
+    }
+
+    public JonSeekerProfile(int userAccountId, Users userId, String firstName, String lastName, String city, String state, String country, String workAuthorizatoin, String employeeType, String resume, String profilePhoto, List<Skills> skills) {
+        this.userAccountId = userAccountId;
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+        this.workAuthorizatoin = workAuthorizatoin;
+        this.employeeType = employeeType;
+        this.resume = resume;
+        this.profilePhoto = profilePhoto;
+        this.skills = skills;
+    }
+
+    public int getUserAccountId() {
+        return userAccountId;
+    }
+
+    public void setUserAccountId(int userAccountId) {
+        this.userAccountId = userAccountId;
+    }
+
+    public Users getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Users userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getWorkAuthorizatoin() {
+        return workAuthorizatoin;
+    }
+
+    public void setWorkAuthorizatoin(String workAuthorizatoin) {
+        this.workAuthorizatoin = workAuthorizatoin;
+    }
+
+    public String getEmployeeType() {
+        return employeeType;
+    }
+
+    public void setEmployeeType(String employeeType) {
+        this.employeeType = employeeType;
+    }
+
+    public String getResume() {
+        return resume;
+    }
+
+    public void setResume(String resume) {
+        this.resume = resume;
+    }
+
+    public String getProfilePhoto() {
+        return profilePhoto;
+    }
+
+    public void setProfilePhoto(String profilePhoto) {
+        this.profilePhoto = profilePhoto;
+    }
+
+    public List<Skills> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<Skills> skills) {
+        this.skills = skills;
+    }
+
+    @Override
+    public String toString() {
+        return "JonSeekerProfile{" +
+                "userAccountId=" + userAccountId +
+                ", userId=" + userId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", country='" + country + '\'' +
+                ", workAuthorizatoin='" + workAuthorizatoin + '\'' +
+                ", employeeType='" + employeeType + '\'' +
+                ", resume='" + resume + '\'' +
+                ", profilePhoto='" + profilePhoto + '\'' +
+                ", skills=" + skills +
+                '}';
+    }
+}
---
+++ src/main/java/com/nitish/hirehub/entity/Skills.java
@@ -0,0 +1,9 @@
+package com.nitish.hirehub.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "skills")
+public class Skills {
+}
